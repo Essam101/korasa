e1bf8a5386ecff2ee5ea7380daa49d443cfc2455
Essam101 <essamsaleh101@gmail.com>
2022-07-04 12:12:20 +0200

    update

---
+++ lib/models/notificationModels/notificationModel.dart
@@ -0,0 +1,81 @@
+// To parse this JSON data, do
+//
+//     final notificationModel = notificationModelFromJson(jsonString);
+
+import 'dart:convert';
+
+class NotificationModel {
+  NotificationModel({
+    required this.topic,
+    required this.data,
+    required this.notification,
+  });
+
+  String topic;
+  Data data;
+  Notification notification;
+
+  factory NotificationModel.fromRawJson(String str) => NotificationModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory NotificationModel.fromJson(Map<String, dynamic> json) => NotificationModel(
+        topic: json["topic"],
+        data: Data.fromJson(json["data"]),
+        notification: Notification.fromJson(json["notification"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "topic": topic,
+        "data": data.toJson(),
+        "notification": notification.toJson(),
+      };
+}
+
+class Data {
+  Data({
+    required this.via,
+    required this.count,
+  });
+
+  String via;
+  String count;
+
+  factory Data.fromRawJson(String str) => Data.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+        via: json["via"],
+        count: json["count"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "via": via,
+        "count": count,
+      };
+}
+
+class Notification {
+  Notification({
+    required this.title,
+    required this.body,
+  });
+
+  String title;
+  String body;
+
+  factory Notification.fromRawJson(String str) => Notification.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Notification.fromJson(Map<String, dynamic> json) => Notification(
+        title: json["title"],
+        body: json["body"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "title": title,
+        "body": body,
+      };
+}
--- lib/services/notification_services.dart
+++ lib/services/notification_services.dart
@@ -1,40 +1,77 @@
 import 'dart:convert';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop/core/instances.dart';
+import 'package:shop/models/notificationModels/notificationModel.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationServices {
+  Instances instances = new Instances();
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  String channelId = "Korasa";
+  String channelName = "Korasa";
+  String channelDescription = "Korasa";
+
   subscribeToTopic({required String topicName}) async {
-    await new Instances().messaging.subscribeToTopic(topicName);
+    await instances.messaging.subscribeToTopic(topicName);
   }
 
   unsubscribeFromTopic({required String topicName}) async {
-    await new Instances().messaging.unsubscribeFromTopic(topicName);
+    await instances.messaging.unsubscribeFromTopic(topicName);
   }
 
-  Future<void> sendPushNotification({
-    required topicName,
-  }) async {
+  Future<void> sendPushNotification({required NotificationModel notificationModel}) async {
     try {
       await http.post(
         Uri.parse('https://api.rnfirebase.io/messaging/send'),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
-        body: jsonEncode({
-          'topic': "",
-          'data': {
-            'via': 'FlutterFire Cloud Messaging!!!',
-            'count': "1",
-          },
-          'notification': {
-            'title': 'Hello FlutterFire!',
-            'body': 'This notification  was created via FCM!',
-          },
-        }),
+        body: notificationModel.toJson(),
       );
       print('FCM request for device sent!');
     } catch (e) {
       print(e);
     }
+  }
+
+  getInitialMessage({required Function action}) {
+    instances.messaging.getInitialMessage().then((RemoteMessage? message) {
+      if (message != null) {
+        action();
+      }
+    });
+  }
+
+  onMessage({required Function action}) {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null && !kIsWeb) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(channelId, channelName,
+                channelDescription: channelDescription, icon: 'launch_background', playSound: true, priority: Priority.high),
+          ),
+        );
+        action();
+      }
+    });
+  }
+
+  onMessageOpenedApp({required Function action}) {
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      action();
+      //   print('A new onMessageOpenedApp event was published!');
+      // Navigator.pushNamed(
+      //   context,
+      //   '/message',
+      //   arguments: MessageArguments(message, true),
+      // );
+    });
   }
 }
